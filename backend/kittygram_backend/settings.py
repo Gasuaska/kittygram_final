b989052e263f0cfbfcec34cd1efdf8614416d495
Gasuaska <gasuaska2@gmail.com>
2025-07-28 17:36:52 +0500

    i'm very tired

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,10 +8,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='some-fallback-secret')
 
 DEBUG = os.getenv('DEBUG', default='True').lower == 'true'
 
-# allowed_hosts = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
-# ALLOWED_HOSTS = [host.strip() for host in allowed_hosts]
-
-ALLOWED_HOSTS=['127.0.0.1', 'localhost', '84.201.176.175', 'gasuaska-kittygram.zapto.org']
+allowed_hosts = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS]
 
 INSTALLED_APPS = [
     'django.contrib.admin',

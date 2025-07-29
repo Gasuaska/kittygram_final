ae273cb1a8a06d19fab462eaf3da48ff01eb120c
Gasuaska <gasuaska2@gmail.com>
2025-07-29 06:27:49 +0500

    i'm very tired

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -97,10 +97,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/backend_static/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_ROOT = '/app/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
